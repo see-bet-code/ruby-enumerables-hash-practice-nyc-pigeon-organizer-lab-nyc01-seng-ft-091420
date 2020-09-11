28aab797ef268f62a64654940c6cc6315fa88e2c
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 13:55:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   data.each { |key, hash|
+    binding.pry
     hash.each { |attributes, names|
+      binding.pry
       new_hash[names] = {
+        binding.pry
         attributes => 1
       }
 
