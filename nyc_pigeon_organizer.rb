f0c8117039b9b455ad5c345268995051b7927d99
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-11 14:14:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,12 +4,13 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
+  info = []
   data.each { |key, hash|
     hash.each { |attributes, names|
-      names.each {
+      names.each { |name|
         binding.pry
-        new_hash[names] = {
-          key => attributes
+        new_hash[name] = {
+          key => info << attributes
         }
       }
     }
